7e136e0b031f651b944dd23a91cfe38bc5d82c91
Kev-Villegas <kevin.villegas1973@gmail.com>
2023-10-10 16:50:26 -0300

    adding breakpoints to tw and Navbar component

--- front/sharp-pizza/src/ui/Layout.jsx
+++ front/sharp-pizza/src/ui/Layout.jsx
@@ -1,17 +1,19 @@
-import { Outlet } from "react-router-dom"
-import CartOverview from "../features/cart/CartOverview"
-import Header from "./Header"
+import { Outlet } from 'react-router-dom';
+import CartOverview from '../features/cart/CartOverview';
+import Header from './Header';
+import Navbar from './Navbar';
 
 const Layout = () => {
   return (
     <>
+      <Navbar />
       <Header />
       <main>
         <Outlet />
       </main>
       <CartOverview />
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
