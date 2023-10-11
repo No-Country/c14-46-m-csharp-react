d44fed31db805dd9df5d691537d57ef8fb843fed
Emilio Pino <pinoen@yahoo.com.ar>
2023-10-11 13:57:46 -0300

    create state slice

---
+++ front/sharp-pizza/src/features/user/userSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+  username: "Nicolas",
+}
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    updateUser: (state, action) => {
+      state.username = action.payload
+    },
+  },
+})
+
+export const { updateUser } = userSlice.actions
+
+export default userSlice.reducer
