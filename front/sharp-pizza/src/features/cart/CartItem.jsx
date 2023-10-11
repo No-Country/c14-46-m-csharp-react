c554a3e0496daded5b3596b6cf7701c76fffe534
Emilio Pino <pinoen@yahoo.com.ar>
2023-10-11 16:24:59 -0300

    add number of unit for each pizza

--- front/sharp-pizza/src/features/cart/CartItem.jsx
+++ front/sharp-pizza/src/features/cart/CartItem.jsx
@@ -4,7 +4,7 @@
 const CartItem = ({ product }) => {
   return (
     <div key={product.id} className="flex justify-between ml-10 items-center max-w-sm">
-      <h2>{product.name}</h2> <span>${product.unitPrice}</span>
+      <h2>1 x {product.name}</h2> <span>${product.unitPrice}</span>
 
     </div>
   )
