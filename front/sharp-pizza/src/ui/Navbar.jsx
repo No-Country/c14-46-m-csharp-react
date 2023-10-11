9a31b793121844a6bfff74eae91b990932a3c6c5
Kev-Villegas <kevin.villegas1973@gmail.com>
2023-10-11 07:44:34 -0300

    fixing some font-sizes for different devices and adding some styles

--- front/sharp-pizza/src/ui/Navbar.jsx
+++ front/sharp-pizza/src/ui/Navbar.jsx
@@ -9,12 +9,12 @@ const Navbar = () => {
     <div className='pl-10 fixed w-full h-[70px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
         <a>
-          <h3 className='cursor-pointer'>Logo</h3>
+          <h3 className='cursor-pointer font-bold'>Logo</h3>
         </a>
       </div>
 
       {/* Menu */}
-      <ul className='hidden md:flex'>
+      <ul className='hidden md:flex text-lg'>
         <li className='cursor-pointer mr-4'>
           <a>Inicio</a>
         </li>
@@ -42,28 +42,25 @@ const Navbar = () => {
 
       {/* Mobile menu */}
       <ul
-        className={
-          !nav
-            ? 'hidden'
-            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+        className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
         }
       >
-        <li className='py-6 text-4xl'>
+        <li className='py-6 text-2xl'>
           <a>Inicio</a>
         </li>
-        <li className='py-6 text-4xl'>
+        <li className='py-6 text-2xl'>
           <a>Nosotros</a>
         </li>
-        <li className='py-6 text-4xl'>
+        <li className='py-6 text-2xl'>
           <a>Menú</a>
         </li>
-        <li className='py-6 text-4xl'>
+        <li className='py-6 text-2xl'>
           <a>Localización</a>
         </li>
-        <li className='py-6 text-4xl'>
+        <li className='py-6 text-2xl'>
           <a>Contacto</a>
         </li>
-        <li className='py-6 text-4xl'>
+        <li className='py-6 text-2xl'>
           <a>Login</a>
         </li>
       </ul>
