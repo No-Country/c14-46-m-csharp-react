503322dfbdf069f8a82be58640a90ab1b4088663
Emilio Pino <pinoen@yahoo.com.ar>
2023-10-11 16:33:04 -0300

    delete navbar component

--- front/sharp-pizza/src/ui/Layout.jsx
+++ front/sharp-pizza/src/ui/Layout.jsx
@@ -1,12 +1,10 @@
 import { Outlet } from 'react-router-dom';
 import CartOverview from '../features/cart/CartOverview';
 import Header from './Header';
-import Navbar from './Navbar';
 
 const Layout = () => {
   return (
     <>
-      <Navbar />
       <Header />
       <main>
         <Outlet />
