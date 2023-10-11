3ef01884245c6e3e1d9029973eae4ec6cf473609
Emilio Pino <pinoen@yahoo.com.ar>
2023-10-11 14:00:00 -0300

    create component

--- front/sharp-pizza/src/ui/Home.jsx
+++ front/sharp-pizza/src/ui/Home.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom"
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className="flex flex-col justify-center items-center h-screen gap-2">
+      <h1 className="text-3xl">Bienvenido a Sharp Pizza</h1>
+      <Link to={'/login'} className="btn btn-primary">Iniciar sesion</Link>
+      <p className="text-xl font-bold">No tenes cuenta?</p>
+      <Link to={'/signup'} className="btn btn-secondary">Crear cuenta</Link>
     </div>
   )
 }
