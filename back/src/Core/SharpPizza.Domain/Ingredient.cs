cfc8b36dd2ca68b846f2345e2c3d2b70ad40969f
Daniel <daniel.serrano0119@gmail.com>
2023-10-11 16:49:10 -0600

    Agregamos la clase Ingredient

---
+++ back/src/Core/SharpPizza.Domain/Ingredient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpPizza.Domain
+{
+    internal class Ingredient
+    {
+        private int _id;
+        private string _name;
+        private bool _available;
+
+        public int Id { get => _id; set => _id = value; }
+        public string Name { get => _name; set => _name = value; }
+        public bool Available { get => _available; set => _available = value; }
+    }
+}
