66b5e11d723e70162eca78545b40074dd6f024a0
Emilio Pino <pinoen@yahoo.com.ar>
2023-10-12 19:13:27 -0300

    add get Users function

--- front/sharp-pizza/src/services/apiPizza.js
+++ front/sharp-pizza/src/services/apiPizza.js
@@ -10,3 +10,14 @@ export const getMenu = async () => {
   const data = await response.json()
   return data
 }
+
+export const getUsers = async () => {
+  const response = await fetch(`${API_URL}/user`)
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch users")
+  }
+
+  const data = await response.json()
+  return data
+}
