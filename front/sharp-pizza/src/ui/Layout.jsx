95da46ee33545ca3176626ab90fea803909542b0
Emilio Pino <pinoen@yahoo.com.ar>
2023-10-12 19:13:51 -0300

    add condition to render component and toast component

--- front/sharp-pizza/src/ui/Layout.jsx
+++ front/sharp-pizza/src/ui/Layout.jsx
@@ -1,15 +1,22 @@
 import { Outlet } from 'react-router-dom';
 import CartOverview from '../features/cart/CartOverview';
 import Header from './Header';
+import { useSelector } from 'react-redux';
+import { Toaster } from 'react-hot-toast';
+
 
 const Layout = () => {
+
+  const products = useSelector(state => state.cart.products);
   return (
     <>
       <Header />
+      <Toaster toastOptions={{ duration: 3000 }} />
+
       <main className='m-20'>
         <Outlet />
       </main>
-      <CartOverview />
+      {products.length > 0 && <CartOverview />}
     </>
   );
 };
