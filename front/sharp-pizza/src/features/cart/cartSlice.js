c599c0e8c157c1ab33e8944db8d56d77ad29fc65
Emilio Pino <pinoen@yahoo.com.ar>
2023-10-17 15:03:47 -0300

    add reducers

--- front/sharp-pizza/src/features/cart/cartSlice.js
+++ front/sharp-pizza/src/features/cart/cartSlice.js
@@ -2,7 +2,6 @@ import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
   products: [],
-  total: 0
 }
 
 const cartSlice = createSlice({
@@ -11,10 +10,31 @@ const cartSlice = createSlice({
   reducers: {
     addProduct: (state, action) => {
       state.products.push(action.payload)
-      state.total += action.payload.unitPrice
+    },
+    deleteProduct: (state, action) => {
+      state.products = state.products.filter(product => product.id !== action.payload)
+    },
+    increaseQuantity: (state, action) => {
+      const product = state.products.find(product => product.id === action.payload)
+      product.quantity++
+    },
+    decreaseQuantity: (state, action) => {
+      const product = state.products.find(product => product.id === action.payload)
+      product.quantity--
+    },
+    clearCart: (state) => {
+      state.products = []
     }
   }
 })
 
-export const { addProduct } = cartSlice.actions
+export const { addProduct, deleteProduct, increaseQuantity, decreaseQuantity, clearCart } = cartSlice.actions
 export default cartSlice.reducer
+
+export const getTotalQuantity = (state) => {
+  return state.cart.products.reduce((acc, product) => acc + product.quantity, 0)
+}
+
+export const getTotalPrice = (state) => {
+  return state.cart.products.reduce((acc, product) => acc + product.totalPrice, 0)
+}
