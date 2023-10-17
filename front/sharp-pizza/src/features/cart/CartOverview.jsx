319a02c13c4cdce9c7485126cd30e68738b412b1
Emilio Pino <pinoen@yahoo.com.ar>
2023-10-17 15:04:18 -0300

    get price and quantity from cart slice

--- front/sharp-pizza/src/features/cart/CartOverview.jsx
+++ front/sharp-pizza/src/features/cart/CartOverview.jsx
@@ -1,19 +1,20 @@
 import { useSelector } from "react-redux"
 import { Link } from "react-router-dom"
 import { BsFillCartPlusFill } from "react-icons/bs"
+import { getTotalPrice, getTotalQuantity } from "./cartSlice"
 
 const CartOverview = () => {
 
-  const products = useSelector(state => state.cart.products)
-  const total = useSelector(state => state.cart.total)
+  const totalQuantity = useSelector(getTotalQuantity)
+  const totalPrice = useSelector(getTotalPrice)
 
   return (
     <div className="flex justify-between px-10 w-full items-center fixed bottom-0 h-20 bg-[#0a192f] font-bold" >
       <p className="flex gap-2">
-        <span className="">{products.length} pizzas -</span>
-        <span>${total}</span>
+        <span className="">{totalQuantity} pizzas -</span>
+        <span>${totalPrice}</span>
       </p>
-      <div className="flex justify-center items-center gap-2"><Link to={'/cart'}>Open cart &rarr; <BsFillCartPlusFill size={25} /></Link></div>
+      <div className="flex justify-center items-center gap-2"><Link to={'/cart'}>Ir al carrito &rarr; <BsFillCartPlusFill size={25} /></Link></div>
     </div>
   )
 }
