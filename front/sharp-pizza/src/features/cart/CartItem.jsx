3089760c925ee24f30b4ffd619a184d908e508eb
Emilio Pino <pinoen@yahoo.com.ar>
2023-10-17 15:04:47 -0300

    add delete btn

--- front/sharp-pizza/src/features/cart/CartItem.jsx
+++ front/sharp-pizza/src/features/cart/CartItem.jsx
@@ -1,10 +1,19 @@
 /* eslint-disable react/prop-types */
 
+import { useDispatch } from "react-redux"
+import { deleteProduct } from "./cartSlice"
+
 
 const CartItem = ({ product }) => {
+  const dispatch = useDispatch()
+
+
   return (
-    <div key={product.id} className="flex justify-between ml-10 items-center max-w-sm">
+    <div className="flex justify-between ml-10 items-center max-w-sm">
       <h2>1 x {product.name}</h2> <span>${product.unitPrice}</span>
+
+      <button className="btn btn-warning w-20" onClick={() => dispatch(deleteProduct(product.id))}>Quitar</button>
+
 
     </div>
   )
