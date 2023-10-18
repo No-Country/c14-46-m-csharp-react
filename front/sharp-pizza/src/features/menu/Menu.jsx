c4fe73d85dc9d9df49444a844332145fcbbe207e
Kev-Villegas <kevin.villegas1973@gmail.com>
2023-10-18 16:11:59 -0300

    improving the card design a little

--- front/sharp-pizza/src/features/menu/Menu.jsx
+++ front/sharp-pizza/src/features/menu/Menu.jsx
@@ -1,28 +1,26 @@
-import { useEffect, useState } from "react"
-import MenuItem from "./MenuItem"
-import { getMenu } from "../../services/apiPizza"
+import { useEffect, useState } from 'react';
+import MenuItem from './MenuItem';
+import { getMenu } from '../../services/apiPizza';
 
 const Menu = () => {
-
-  const [menu, setMenu] = useState([])
+  const [menu, setMenu] = useState([]);
 
   useEffect(() => {
-    getMenu().then(data => {
-      setMenu(data)
-    })
-  }, [])
-
+    getMenu().then((data) => {
+      setMenu(data);
+    });
+  }, []);
 
   return (
-    <main className="ml-10">
-      <h1>Menu</h1>
-      <div className='flex flex-wrap gap-10'>
-        {menu.map(item => (
+    <main className='mx-10'>
+      <h1 className='text-3xl text-center py-7'>Nuestro Menú</h1>
+      <div className='grid sm:grid-cols-1 2xl:px-4 sm:justify-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 justify-between gap-8'>
+        {menu.map((item) => (
           <MenuItem key={item.id} item={item} />
         ))}
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
