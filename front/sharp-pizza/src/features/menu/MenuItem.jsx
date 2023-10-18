379912a9a07f766d65cc33200ceee9bde91be573
Kev-Villegas <kevin.villegas1973@gmail.com>
2023-10-18 16:13:14 -0300

    First letter of the ingredients in capital letters and improving some styles

--- front/sharp-pizza/src/features/menu/MenuItem.jsx
+++ front/sharp-pizza/src/features/menu/MenuItem.jsx
@@ -1,34 +1,48 @@
-/* eslint-disable react/prop-types */
+import { useDispatch } from 'react-redux';
+import toast from 'react-hot-toast';
 
-import { useDispatch } from "react-redux"
-import { addProduct } from "../cart/cartSlice"
+import { addProduct } from '../cart/cartSlice';
 
 const MenuItem = ({ item }) => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const newItem = {
     id: item.id,
     name: item.name,
     quantity: 1,
     unitPrice: item.unitPrice,
-    totalPrice: item.unitPrice * 1
-  }
+    totalPrice: item.unitPrice * 1,
+  };
+
+  const addToCart = () => {
+    dispatch(addProduct(newItem));
+    toast.success('Producto agregado!');
+  };
 
   return (
-    <div className="card w-50 bg-base-100 shadow-xl">
-      <figure><img src={item.imageUrl} alt={item.name} /></figure>
-      <div className="card-body">
-        <h2 className="card-title">{item.name}</h2>
-        <p className="font-bold">${item.unitPrice}</p>
-        {item.ingredients.map(ingredient => (
-          <p className="text-sm h-2 py-2 mb-2" key={ingredient}>{ingredient}</p>
+    <div className='card w-50 bg-base-100 shadow-xl'>
+      <figure>
+        <img src={item.imageUrl} alt={item.name} className=' rounded-md' />
+      </figure>
+      <div className='card-body'>
+        <h2 className='card-title text-2xl font-bold'>{item.name}</h2>
+        <p className='font-bold text-xl'>${item.unitPrice}</p>
+        {item.ingredients.map((ingredient) => (
+          <p className='text-sm h-2 py-2 mb-2' key={ingredient}>
+            {ingredient.charAt(0).toUpperCase() + ingredient.slice(1)}
+          </p>
         ))}
-        <div className="card-actions">
-          <button className="btn btn-primary btn-xs" onClick={() => dispatch(addProduct(newItem))}>Agregar al carrito</button>
+        <div className='card-actions justify-start'>
+          <button
+            className='btn btn-active btn-neutral my-4'
+            onClick={addToCart}
+          >
+            Agregar al carrito
+          </button>
         </div>
       </div>
-    </div >
-  )
-}
+    </div>
+  );
+};
 
-export default MenuItem
+export default MenuItem;
