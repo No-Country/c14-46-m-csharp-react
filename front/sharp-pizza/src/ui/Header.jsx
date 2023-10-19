3c904e26783d49fb56b90616b930e956a12bf0c2
Emilio Pino <pinoen@yahoo.com.ar>
2023-10-19 00:56:10 -0300

    add logout btn and its functionality

--- front/sharp-pizza/src/ui/Header.jsx
+++ front/sharp-pizza/src/ui/Header.jsx
@@ -17,17 +17,58 @@ const Header = () => {
   }
 
   return (
-    <header className="flex fixed top-0 mb-10 justify-between items-center bg-[#0a192f] h-20 w-full px-10 z-10">
-      <Link to={'/'} className="text-3xl font-bold">Sharp Pizza</Link>
-      <input type="text" placeholder="Buscar pedido #" className="input input-bordered w-full max-w-xs" />
-
-      <div className="flex justify-center items-center gap-2">
-        <Username />
-        {username && <HiOutlineLogout size={25} onClick={handleLogout} />}
+    <div className='flex fixed top-0 pl-10 w-full h-[80px] justify-between items-center px-6 bg-[#0a192f] text-gray-300'>
+      <div>
+        <a href='#' className='cursor-pointer'>
+          <Link to='/' className='text-2xl'>Sharp Pizza</Link>
+        </a>
       </div>
 
-    </header>
-  )
+      <ul className='hidden md:flex space-x-6 items-center'>
+        <li>
+          <a href='#' onClick={toggleSearch} className='flex items-center'>
+            <FiSearch
+              className={`ml-2 hover:text-blue-500 ${searchOpen ? 'w-12' : 'w-6'
+                } transition-all duration-3000 ease-in-out`}
+              size={22}
+            />
+            {searchOpen && (
+              <input
+                type='text'
+                placeholder='Busca Tu Pizza...'
+                className='input input-bordered input-sm w-full max-w-xs ml-2'
+                onClick={handleInputClick}
+              />
+            )}
+          </a>
+        </li>
+        <li>
+          <Link to='/cart' href='#' className='hover:text-blue-500 flex items-center'>
+            <BsCart className='ml-2' size={22} />
+          </Link>
+        </li>
+        <li>
+          <Link to='/login' className='hover:text-blue-500 flex items-center'>
+            Iniciar Sesión
+            <BiLogIn className='ml-2' size={20} />
+          </Link>
+        </li>
+      </ul>
+
+      {/* Hamburger */}
+      <div onClick={handleClick} className='md:hidden z-10'>
+        {!nav ? <FaBars /> : <FaTimes />}
+        <header className="flex fixed top-0 mb-10 justify-between items-center bg-[#0a192f] h-20 w-full px-10 z-10">
+          <Link to={'/'} className="text-3xl font-bold">Sharp Pizza</Link>
+          <input type="text" placeholder="Buscar pedido #" className="input input-bordered w-full max-w-xs" />
+
+          <div className="flex justify-center items-center gap-2">
+            <Username />
+            {username && <HiOutlineLogout size={25} onClick={handleLogout} />}
+          </div>
+
+        </header>
+        )
 }
 
-export default Header
+        export default Header
