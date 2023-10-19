e7311d754f9f1e8a9bed870b97d9715da9405034
Kev-Villegas <kevin.villegas1973@gmail.com>
2023-10-19 18:18:35 -0300

    Building a new Navbar

--- front/sharp-pizza/src/ui/Header.jsx
+++ front/sharp-pizza/src/ui/Header.jsx
@@ -1,16 +1,90 @@
-import { Link } from "react-router-dom"
-import Username from "../features/user/Username"
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { useState } from 'react';
+import { BiLogIn } from 'react-icons/bi';
+import { BsCart } from 'react-icons/bs';
+import { FiSearch } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
+  const [searchOpen, setSearchOpen] = useState(false);
+  const toggleSearch = () => {
+    setSearchOpen(!searchOpen);
+  };
+
+  const handleInputClick = (e) => {
+    e.stopPropagation(); // Evita que se propague el evento de click al contenedor
+  };
+
   return (
-    <header className="flex fixed top-0 mb-10 justify-between items-center bg-[#0a192f] h-20 w-full px-10 z-10">
-      <Link to={'/'} className="text-3xl font-bold">Sharp Pizza</Link>
-      <input type="text" placeholder="Buscar pedido #" className="input input-bordered w-full max-w-xs" />
+    <div className='flex fixed top-0 pl-10 w-full h-[80px] justify-between items-center px-6 bg-[#0a192f] text-gray-300'>
+      <div>
+        <a href='#' className='cursor-pointer'>
+          <Link to='/' className='text-2xl'>Sharp Pizza</Link>
+        </a>
+      </div>
 
-      <Username />
+      <ul className='hidden md:flex space-x-6 items-center'>
+        <li>
+          <a href='#' onClick={toggleSearch} className='flex items-center'>
+            <FiSearch
+              className={`ml-2 hover:text-blue-500 ${
+                searchOpen ? 'w-12' : 'w-6'
+              } transition-all duration-3000 ease-in-out`}
+              size={22}
+            />
+            {searchOpen && (
+              <input
+                type='text'
+                placeholder='Busca Tu Pizza...'
+                className='input input-bordered input-sm w-full max-w-xs ml-2'
+                onClick={handleInputClick}
+              />
+            )}
+          </a>
+        </li>
+        <li>
+          <Link to='/cart' href='#' className='hover:text-blue-500 flex items-center'>
+            <BsCart className='ml-2' size={22} />
+          </Link>
+        </li>
+        <li>
+          <Link to='/login' className='hover:text-blue-500 flex items-center'>
+            Iniciar Sesión
+            <BiLogIn className='ml-2' size={20} />
+          </Link>
+        </li>
+      </ul>
 
-    </header>
-  )
-}
+      {/* Hamburger */}
+      <div onClick={handleClick} className='md:hidden z-10'>
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
 
-export default Header
+      {/* Mobile menu */}
+      <ul
+        className={
+          !nav
+            ? 'hidden'
+            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+        }
+      >
+        <li className='py-6 text-4xl'>
+          <a href='#' className='hover:text-blue-500'>
+            Home
+          </a>
+        </li>
+
+        <li className='py-6 text-4xl'>
+          <a href='#' className='hover:text-blue-500'>
+            Login
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
