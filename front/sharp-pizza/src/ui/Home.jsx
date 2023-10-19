965f9300cae6f4775c6fe24b5b062bf0a7042c0d
Kev-Villegas <kevin.villegas1973@gmail.com>
2023-10-19 18:18:19 -0300

    Building a New Home

--- front/sharp-pizza/src/ui/Home.jsx
+++ front/sharp-pizza/src/ui/Home.jsx
@@ -1,18 +1,27 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-screen gap-2">
-      <h1 className="text-3xl">Bienvenido a Sharp Pizza</h1>
-      <Link to={'/login'} className="btn btn-primary">Iniciar sesion</Link>
-      <p className="text-xl font-bold">No tenes cuenta?</p>
-      <Link to={'/register'} className="btn btn-secondary">Crear cuenta</Link>
-
-      <p className="text-xl font-bold">o simplemente ...</p>
-
-      <Link to={'/menu'} className="btn btn-secondary">Ver menu</Link>
+    <div className='w-full h-screen bg-[#0a192f]'>
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <h2 className='text-4xl sm:text-5xl text-left font-bold text-[#f77f00]'>
+          Sharp Pizza
+        </h2>
+        <h1 className='mt-4 text-4xl sm:text-5xl text-left font-bold text-[#bfdbfe]'>
+          Las Mejores Pizzas Con Los Mejores Ingredientes
+        </h1>
+        <div className='mt-8'>
+          <Link
+            to='/menu'
+            className='text-white border-2 px-4 py-2 my-2 items-center rounded-lg mt-6'
+          >
+            Mirar El Menú
+          </Link>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
