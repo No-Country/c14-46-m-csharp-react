0406bcccab428bf0cb8de57d750728feceff8b27
Emilio Pino <pinoen@yahoo.com.ar>
2023-10-21 00:45:29 -0300

    add gif as a bg image

--- front/sharp-pizza/src/ui/Home.jsx
+++ front/sharp-pizza/src/ui/Home.jsx
@@ -2,15 +2,24 @@ import { Link } from "react-router-dom"
 
 const Home = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-screen gap-2">
-      <h1 className="text-3xl">Bienvenido a Sharp Pizza</h1>
-      <Link to={'/login'} className="btn btn-primary">Iniciar sesion</Link>
-      <p className="text-xl font-bold">No tenes cuenta?</p>
-      <Link to={'/register'} className="btn btn-secondary">Crear cuenta</Link>
+    <div className='w-full h-screen bg-[#0a192f] gif'>
+      <div className='max-w-md mx-auto px-8 flex flex-col justify-center items-center h-full'>
+        <h2 className='text-8xl sm:text-5xl font-bold text-slate-900 underline tracking-wide hover:text-lime-950 cursor-progress'>
+          SHARP PIZZA
+        </h2>
 
-      <p className="text-xl font-bold">o simplemente ...</p>
-
-      <Link to={'/menu'} className="btn btn-secondary">Ver menu</Link>
+        <h1 className='mt-4 text-4xl sm:text-5xl text-center font-bold text-lime-950 tracking-wider hover:text-slate-900'>
+          Las mejores pizzas con los mejores ingredientes
+        </h1>
+        <div className='mt-8'>
+          <Link
+            to='/menu'
+            className='btn btn-accent btn-lg w-52 mt-10'
+          >
+            VER MENU
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
