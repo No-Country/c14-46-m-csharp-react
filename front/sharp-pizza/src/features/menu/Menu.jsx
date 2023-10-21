b49c2220107bc8ebb572f5bd75b1a54e425403db
Emilio Pino <pinoen@yahoo.com.ar>
2023-10-21 00:42:51 -0300

    change bg color and comment by now h1 title

--- front/sharp-pizza/src/features/menu/Menu.jsx
+++ front/sharp-pizza/src/features/menu/Menu.jsx
@@ -12,9 +12,9 @@ const Menu = () => {
   }, [])
 
   return (
-    <main className='mx-10'>
-      <h1 className='text-3xl text-center py-7'>Nuestro Menú</h1>
-      <div className='grid sm:grid-cols-1 2xl:px-4 sm:justify-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 justify-between gap-8'>
+    <main className='bg-stone-100'>
+      {/* <h1 className='text-5xl text-center pt-24 text-stone-900 font-bold tracking-widest underline'>NUESTRO MENU</h1> */}
+      <div className='grid sm:grid-cols-1 2xl:px-4 sm:justify-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-10 p-12 m-12'>
         {menu.map((item) => (
           <MenuItem key={item.id} item={item} />
         ))}
