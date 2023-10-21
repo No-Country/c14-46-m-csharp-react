d6e4bd1bd92573640e4f0bf534cf30c83c5164e5
Emilio Pino <pinoen@yahoo.com.ar>
2023-10-21 00:46:29 -0300

    make logo text bolder and add cart icon

--- front/sharp-pizza/src/ui/Header.jsx
+++ front/sharp-pizza/src/ui/Header.jsx
@@ -4,6 +4,7 @@ import { HiOutlineLogout } from "react-icons/hi"
 import { useDispatch, useSelector } from "react-redux"
 import { logout } from "../features/user/userSlice"
 import { clearCart } from "../features/cart/cartSlice"
+import { BsFillCartPlusFill } from "react-icons/bs"
 
 const Header = () => {
   const username = useSelector(state => state.user.name)
@@ -18,12 +19,13 @@ const Header = () => {
 
   return (
     <header className="flex fixed top-0 mb-10 justify-between items-center bg-[#0a192f] h-20 w-full px-10 z-10">
-      <Link to={'/'} className="text-3xl font-bold">Sharp Pizza</Link>
+      <Link to={'/'} className="text-3xl font-bold">SHARP PIZZA</Link>
       <input type="text" placeholder="Buscar pedido #" className="input input-bordered w-full max-w-xs" />
 
-      <div className="flex justify-center items-center gap-2">
+      <div className="flex justify-center items-center gap-4">
+        <Link to={'/cart'}><BsFillCartPlusFill size={25} /></Link>
         <Username />
-        {username && <HiOutlineLogout size={25} onClick={handleLogout} />}
+        {username && <HiOutlineLogout size={25} onClick={handleLogout} className="cursor-pointer" />}
       </div>
 
     </header>
