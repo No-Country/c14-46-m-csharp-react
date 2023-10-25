94cc3f45576ec38eadbd22b2046d24810b641c3b
Kev-Villegas <kevin.villegas1973@gmail.com>
2023-10-25 15:21:54 -0300

    Updating the main menu text

--- front/sharp-pizza/src/ui/Home.jsx
+++ front/sharp-pizza/src/ui/Home.jsx
@@ -1,27 +1,27 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div className='w-full h-screen bg-[#0a192f] gif'>
-      <div className='max-w-md mx-auto px-8 flex flex-col justify-center items-center h-full'>
-        <h2 className='text-8xl sm:text-5xl font-bold text-slate-900 underline tracking-wide hover:text-lime-950 cursor-progress'>
-          SHARP PIZZA
+    <div name='home' className='w-full h-screen bg-[#0a192f] gif'>
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p className='text-stone-500 text-4xl'>Bienvenidos A</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-stone-400'>
+          Sharp Pizza
+        </h1>
+        <h2 className='text-4xl sm:text-7xl font-bold text-stone-400'>
+          Las Mejores Pizzas Con Los Mejores Ingredientes
         </h2>
 
-        <h1 className='mt-4 text-4xl sm:text-5xl text-center font-bold text-lime-950 tracking-wider hover:text-slate-900'>
-          Las mejores pizzas con los mejores ingredientes
-        </h1>
-        <div className='mt-8'>
-          <Link
-            to='/menu'
-            className='btn btn-accent btn-lg w-52 mt-10'
-          >
-            VER MENU
-          </Link>
-        </div>
+        <Link to='/menu'>
+          <button className='rounded-md text-center text-white group border-2 px-6 py-3 my-2 mt-4 flex items-center hover:bg-blue-600 hover:border-blue-600'>
+            Nuestro Menú
+            <span className='group-hover:rotate-90 duration-300'></span>
+          </button>
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
