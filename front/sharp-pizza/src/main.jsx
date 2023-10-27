fc0a90ccc8c10098a882ad134a77323fc28352cc
Emilio Pino <pinoen@yahoo.com.ar>
2023-10-26 22:34:48 -0300

    change toast messages position

--- front/sharp-pizza/src/main.jsx
+++ front/sharp-pizza/src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
-      <Toaster toastOptions={{ duration: 3000 }} />
+      <Toaster position='bottom-center' toastOptions={{ duration: 3000 }} />
     </Provider>
   </React.StrictMode>,
 )
