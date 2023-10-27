77a00508d3ce7f152e9c9da6ba3150044a118b22
Emilio Pino <pinoen@yahoo.com.ar>
2023-10-27 09:58:55 -0300

    add search by orderid feature

--- front/sharp-pizza/src/ui/Header.jsx
+++ front/sharp-pizza/src/ui/Header.jsx
@@ -6,11 +6,13 @@ import { logout } from '../features/user/userSlice';
 import { clearCart } from '../features/cart/cartSlice';
 import { BsFillCartPlusFill } from 'react-icons/bs';
 import toast from 'react-hot-toast';
+import { useState } from 'react';
 
 const Header = () => {
   const username = useSelector((state) => state.user.name);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [query, setQuery] = useState('');
 
   const handleLogout = () => {
     dispatch(logout());
@@ -18,6 +20,12 @@ const Header = () => {
     navigate('/');
     toast.success('Sesión cerrada! Vuelva pronto.');
   };
+
+  const handleSearch = (e) => {
+    if (e.key === 'Enter') {
+      navigate(`/order/${query}`)
+    }
+  }
 
   return (
     <header className='flex fixed top-0 mb-10 justify-between items-center bg-[#0a192f] h-20 w-full px-10 z-10 '>
@@ -28,6 +36,9 @@ const Header = () => {
         type='text'
         placeholder='Busca Un Pedido...'
         className='input input-bordered w-full max-w-xs'
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyDown={handleSearch}
       />
 
       <div className='flex justify-center items-center gap-4'>
