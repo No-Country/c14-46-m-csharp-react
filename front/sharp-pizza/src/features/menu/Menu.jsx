93a49fb476c584af6876302c82d7394f501fcb47
Emilio Pino <pinoen@yahoo.com.ar>
2023-10-28 00:59:11 -0300

    change id property name to _id as is how it comes from the database

--- front/sharp-pizza/src/features/menu/Menu.jsx
+++ front/sharp-pizza/src/features/menu/Menu.jsx
@@ -14,7 +14,7 @@ const Menu = () => {
   return (
     <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-8 md:gap-10 px-4 py-20'>
       {menu.map((item) => (
-        <MenuItem key={item.id} item={item} />
+        <MenuItem key={item._id} item={item} />
       ))}
     </div>
   );
