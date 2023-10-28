0530a17ee0931b91d9963b5d18f9f90e69a916a4
Emilio Pino <pinoen@yahoo.com.ar>
2023-10-28 01:04:48 -0300

    create model files with its schemas

---
+++ front/api/models/userModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+// the schema has to have a name, password, phone, email and address
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  }
+})
+
+export const User = mongoose.model("User", userSchema);
