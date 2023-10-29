aa41d2695b4c6be6c48f0f78901a38b1692ce57d
Emilio Pino <pinoen@yahoo.com.ar>
2023-10-29 00:50:32 -0300

    substract 180 minutes (3 hours) in order to fix time zone issue

--- front/sharp-pizza/src/utils/status.js
+++ front/sharp-pizza/src/utils/status.js
@@ -1,5 +1,5 @@
 export const getStatus = (date1, date2) => {
-  const differenceInMinutes = Math.abs(date2 - date1) / (1000 * 60);
+  const differenceInMinutes = Math.abs(date2 - date1) / (1000 * 60) - 180;
   if (differenceInMinutes < 15) {
     return 'en preparacion'
   } else if (differenceInMinutes < 30) {
