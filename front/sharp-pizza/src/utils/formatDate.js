65d3dce9fbd8e205027cb1b4a67e365c702a4e7b
Emilio Pino <pinoen@yahoo.com.ar>
2023-10-29 00:38:34 -0300

    fix timezone offset issue which caused a 3 hours time diference

--- front/sharp-pizza/src/utils/formatDate.js
+++ front/sharp-pizza/src/utils/formatDate.js
@@ -1,12 +1,12 @@
 export function formatDate(inputDate) {
   const date = new Date(inputDate);
 
-  const day = date.getDate().toString().padStart(2, '0');
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const year = date.getFullYear();
-  const hours = date.getHours().toString().padStart(2, '0');
-  const minutes = date.getMinutes().toString().padStart(2, '0');
-  const seconds = date.getSeconds().toString().padStart(2, '0');
+  const day = date.getUTCDate().toString().padStart(2, '0');
+  const month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
+  const year = date.getUTCFullYear();
+  const hours = date.getUTCHours().toString().padStart(2, '0');
+  const minutes = date.getUTCMinutes().toString().padStart(2, '0');
+  const seconds = date.getUTCSeconds().toString().padStart(2, '0');
 
   const formattedDate = `${day}-${month}-${year}, ${hours}:${minutes}:${seconds}`;
   return formattedDate;
