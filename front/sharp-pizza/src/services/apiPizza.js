430ed997bcf9437f7a8e63bc04016612bec6af7e
Emilio Pino <pinoen@yahoo.com.ar>
2023-10-28 23:35:03 -0300

    replace localhost url with one from render

--- front/sharp-pizza/src/services/apiPizza.js
+++ front/sharp-pizza/src/services/apiPizza.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000"
+const API_URL = "https://sharp-pizza-api.onrender.com"
 
 export const getMenu = async () => {
   const response = await fetch(`${API_URL}/menu`)
