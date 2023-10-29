5a661842de54890b37cface5893cc497cd84df8f
Emilio Pino <pinoen@yahoo.com.ar>
2023-10-28 21:01:29 -0300

    add variable environment

--- front/api/config.js
+++ front/api/config.js
@@ -1,2 +1,2 @@
-export const PORT = 3000;
+export const PORT = process.env.PORT || 3000;
 export const mongoURL = "mongodb+srv://pinoen:pinoen@sharp-pizza.o6vfupe.mongodb.net/sharp-pizza?retryWrites=true&w=majority"
