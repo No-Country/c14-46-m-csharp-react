d6885c7003bfff2621da6fe91b4c6f0a4de0ce7a
Kev-Villegas <kevin.villegas1973@gmail.com>
2023-10-30 13:48:49 -0300

    adding colors to tailwind.config.js

--- front/sharp-pizza/tailwind.config.js
+++ front/sharp-pizza/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
@@ -11,6 +12,34 @@ export default {
       lg: '1024px', // Large
       xl: '1280px', // Extra large
       '2xl': '1536px', // Extra extra large
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      rose: colors.rose,
+      pink: colors.pink,
+      fuchsia: colors.fuchsia,
+      purple: colors.purple,
+      violet: colors.violet,
+      indigo: colors.indigo,
+      blue: colors.blue,
+      sky: colors.sky,
+      cyan: colors.cyan,
+      teal: colors.teal,
+      emerald: colors.emerald,
+      green: colors.green,
+      lime: colors.lime,
+      yellow: colors.yellow,
+      amber: colors.amber,
+      orange: colors.orange,
+      red: colors.red,
+      slate: colors.slate,
+      zinc: colors.zinc,
+      gray: colors.gray,
+      neutral: colors.slate,
+      stone: colors.stone,
     },
   },
   plugins: [require('daisyui')],
