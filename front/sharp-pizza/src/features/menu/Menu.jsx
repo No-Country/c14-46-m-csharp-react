73b0444b2b44bf805a1067d0f59e44cde19ff720
Emilio Pino <pinoen@yahoo.com.ar>
2023-10-30 21:18:41 -0300

    add loader component while the menu is loading

--- front/sharp-pizza/src/features/menu/Menu.jsx
+++ front/sharp-pizza/src/features/menu/Menu.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import MenuItem from './MenuItem';
 import { getMenu } from '../../services/apiPizza';
+import Loader from '../../ui/Loader';
 
 const Menu = () => {
   const [menu, setMenu] = useState([]);
@@ -12,7 +13,8 @@ const Menu = () => {
   }, []);
 
   return (
-    <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-8 md:gap-10 px-4 py-20'>
+    <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-8 md:gap-10 px-4 py-20 bg-slate-300'>
+      <Loader />
       {menu.map((item) => (
         <MenuItem key={item._id} item={item} />
       ))}
