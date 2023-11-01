5cdbd58a6f1cd087f2a6f5af359278c29b3e7203
Kev-Villegas <kevin.villegas1973@gmail.com>
2023-11-01 14:44:01 -0300

    Fixing the loader issue

--- front/sharp-pizza/src/features/menu/Menu.jsx
+++ front/sharp-pizza/src/features/menu/Menu.jsx
@@ -11,10 +11,9 @@ const Menu = () => {
       setMenu(data);
     });
   }, []);
-
+  if (!menu.length) return <Loader />;
   return (
     <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-8 md:gap-10 px-4 py-20 bg-slate-300'>
-      <Loader />
       {menu.map((item) => (
         <MenuItem key={item._id} item={item} />
       ))}
